c4466d5ec24634969ecaeef3822ca43408c23e2d
Quinn Elwood <ashes@quinns-mbp-2.lan>
2022-12-04 14:51:06 -0800

    Setup nexting videos

--- services/frontend/components/atoms/VideoEmbed/VideoEmbed.tsx
+++ services/frontend/components/atoms/VideoEmbed/VideoEmbed.tsx
@@ -4,12 +4,13 @@ import styles from "./VideoEmbed.module.css";
 
 export type TVideoEmbedProps = {
   url: string;
+  onEnded?: () => void;
 };
 
-const VideoEmbed = ({ url }: TVideoEmbedProps) => {
+const VideoEmbed = ({ url, onEnded = () => {} }: TVideoEmbedProps) => {
   return (
     <div className={styles.videoEmbed}>
-      <ReactPlayer url={url} playing controls pip />
+      <ReactPlayer url={url} playing controls pip onEnded={onEnded} />
     </div>
   );
 };
